a4dc693647d9c4369c4fc86d2eed0239da2098e4
nonya biznes <slavs12@yahoo.com>
2024-02-14 03:30:46 -0700

    Latest Changes

--- app/src/main/java/com/example/p2_movieviewer/MainActivity.java
+++ app/src/main/java/com/example/p2_movieviewer/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.p2_movieviewer;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.example.p2_movieviewer.databinding.ActivityMainBinding;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
-
-    private final MainActivityViewModel data = new MainActivityViewModel();
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    MaterialToolbar toolbar;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,24 +26,67 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        com.example.p2_movieviewer.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
-
-        Toolbar newToolbar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(newToolbar);
-
+        toolbar = (MaterialToolbar)findViewById(R.id.toolbar);
+        navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        if(actionBar != null)
+        {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        toolbar.setOnMenuItemClickListener(this::onMenuItemClick);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_closed)
+        {
+            @Override
+            public void onDrawerOpened(View drawerView){
+                super.onDrawerOpened(drawerView);
+                Toast.makeText(MainActivity.this, "hurray", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                Toast.makeText(MainActivity.this, "Boo", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        navigationView.setNavigationItemSelectedListener(this);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_app_bar, menu);
-
-
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return PerformMenuItem(item);
+    }
 
+    public boolean onMenuItemClick(MenuItem item) {
+        return PerformMenuItem(item);
+    }
+
+    private boolean PerformMenuItem(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if (id == R.id.home_menu_option)
+        {
+        }
+        else if (id == R.id.library_menu_option)
+        {
+        }
+        else if (id == R.id.movie_info_menu_option)
+        {
+        }
+
+        return true;
+    }
 }
